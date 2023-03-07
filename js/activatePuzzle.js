c8476b522542903d15d669cdc6f12e9f0f41cd00
Simenya Jonathan Enoch <62070508+Simenya@users.noreply.github.com>
2023-03-07 10:00:50 +0300

    Add files via upload

---
+++ js/activatePuzzle.js
@@ -0,0 +1,68 @@
+
+
+var options = {
+    solution: 			[[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	 "C",	null,	null,   null,   null,   null,   null,   null,   null,    null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	 "I",	null,	null,   null,  null,     "E",   null,   null,    null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	  "H",	null,	null,	 "P",	null,	null,   null,   null,    "N",   null,   null,    null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	  "A",	 "U",	 "T",	 "H",	 "E",    "N",   "T",    "I",     "C",    "A",    "T",    "I",    "O",   "N",  null,   null,   null],
+                         [null,	null,	null,	  "S",	null,	null,	 "E",	null,	null,   null,   null,    "R",   null,   null,   null,   null,  null,   null,   null,   null],
+                         [null,	null,	null,	  "H",	null,	null,    "R",   null,	null,   null,   null,    "Y",   null,   null,   null,   null,  null,   null,   null,   null],
+                         [null,	null,	null,	  "E",	null,	null,	null,	null,	null,   null,    "V",    "P",    "N",   null,   null,    "M",   null,   null,   null,   null],
+                         [null,	null,	null,	  "D",	 "E",	 "B",	 "U",	 "G",	null,   null,   null,    "T",   null,   null,   null,    "A",   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,    "I",   null,   null,   null,    "L",   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,    "O",   null,   null,   null,    "W",   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,    "E",	 "N",    "I",   "G",    "M",     "A",   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,    "R",   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,    "E",   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null], 
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	 null,	 null,	null,	null,   null,   null,   null,   null,   null,    null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+
+     index: 			[[null,	null,	 null,	 null,	 null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,      2,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   1,   null,   null,   null,      null,   null,   null,   null,   null],
+                         [null,   null,   null,      3,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,      4,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   6,      null,   null,   null,   null,      8,   null,   null,   null,   null],
+                         [null,   null,   null,      5,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,      5,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,      7,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+  questions_across: [	  
+                           "4. Verifying identity of a process",
+                           "5. Identify and fix errors.",
+                           "6. Secure tunnel.",
+                           "7. Used to protect commercial and Military communication in the mid 20th century."
+                     ],
+
+  questions_down: [		 
+                          "1. Obscuring Data.",
+                          "2. secretive or disguised.",
+                          "3. Output of an algorthim with fixed length irrespective of initial file size.",
+                          "8. Designed to disrupt or damage.",
+                 ]
+
+};
+
+
+cwp_nmsp.activateCWP(document.getElementById("myPuzzle"),false,options);
